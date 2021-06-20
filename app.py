8f2eb33ac8e39daf952011a78c210a5835403ebe
Prakash-Ramanathan1211 <prashprakash1211@gmail.com>
2021-06-20 17:19:14 +0530

    changes done

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'omg, it actually worked for me too and verified by sir !!!!!!1'
+    return 'omg, it actually worked for me !!!!!!1'
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
